f5058e28c747f3300986290698e3ccbe7229ccf5
Roman Singh <romansingh.dev@gmail.com>
2022-01-04 22:46:14 +0000

    monitoring_agent

---
+++ linux_sql/scripts/host_info.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+#Set up arguments
+psql_host=$1
+psql_port=$2
+db_name=$3
+psql_user=$4
+psql_password=$5
+
+#check number of arguments
+if [ $# -ne 5]; then
+  echo "Error: enter the correct number of args"
+  exit 1
+fi
+
+#save number of CPU to a variable
+lscpu_out=`lscpu`
+
+#Save machine statistics
+vmstat_mb=$(vmstat --unit M)
+mem_info=$(cat /proc/meminfo)
+
+#Hardware specification variables
+hostname=$(hostname -f)
+cpu_number= $(echo "$lscpu_out"  | egrep "^CPU\(s\):" | awk '{print $2}' | xargs)
+cpu_architecture= $(echo "$lscpu_out"  | egrep "Architecture:" | awk '{print $2}' | xargs)
+cpu_model= $(echo "$lscpu_out"  | egrep "^Model name:" | awk '{print $3 $4 $5 $6 $7}' | xargs)
+cpu_mhz= $(echo "$lscpu_out"  | egrep "^CPU MHz:" | awk '{print $3}' | xargs)
+l2_cache= $(echo "$lscpu_out"  | egrep "^L2 cache:" | awk '{print $3}' | xargs)
+total_mem=$(echo "$mem_info"  | egrep "^MemTotal:" | awk '{print $2}' | xargs)
+timestamp= $(vmstat -t | tail -n1 | awk '{print $18, $19}' | xargs)
+
+#Find matching id in host_info table
+host_id="(SELECT id FROM host_info WHERE hostname='$hostname')";
+
+#Insert into PSQL host_info.
+insert_stmt="INSERT INTO PUBLIC.host_info(timestamp, total_mem, l2_cache, cpu_mhz, cpu_model, cpu_architecture, cpu_number, hostname )
+VALUES('$timestamp', '$total_mem', '$l2_cache', '$cpu_mhz', '$cpu_model', '$cpu_architecture', '$cpu_number', '$hostname')"
+
+#env variable for psql command
+export PGPASSWORD=$psql_password
+
+#Insert data into a database
+psql -h $psql -p $psql_port -d $db_name -U $psql_user -c "insert_stmt"
+exit $?
+
+
---
+++ linux_sql/scripts/host_usage.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+#Set up arguments
+psql_host=$1
+psql_port=$2
+db_name=$3
+psql_user=$4
+psql_password=$5
+
+#check number of arguments
+if [ $# -ne 5]; then
+  echo "Error: enter the correct number of args"
+  exit 1
+fi
+
+#Save machine statistics to variables
+vmstat_mb=$(vmstat --unit M)
+disk_info=$(df -BM)
+
+#Usage Variables
+hostname= $(hostname -f)
+memory_free=$(echo "$vmstat_mb" | awk '{print $4}'| tail -n1 | xargs)
+cpu_idle=$(echo "$vmstat_mb" | awk '{print $15}'| tail -n1 | xargs)
+cpu_kernel=$(echo "$vmstat_mb" | awk '{print $15}' | tail -n1 | xargs)
+disk_io=$(echo "$vmstat_disk" | awk '{print $10}' | tail -n1 | xargs)
+disk_available=$(echo "$disk_info" | awk '{print $4}' | tail -n1 | xargs)
+disk_available="${disk_available::-1}"
+timestamp=$(vmstat -t | awk '{print $18,$19}' | tail -n1 | xargs)
+
+#Subquery to find matching id in host_info table.
+host_id="(SELECT id FROM host_info WHERE hostname='$hostname')"
+
+#Inserts server usage data into host_usage table
+insert_stmt="INSERT INTO host_usage(hostname, memory_free, cpu_idle, cpu_kernel, disk_io, disk_available, timestamp)
+VALUES('$hostname', '$memory_free', '$cpu_idle', '$cpu_kernel', '$disk_io', '$disk_available', '$timestamp')"
+
+export PGPASSWORD=$psql_password
+#Insert date into a database
+psql -h $psql_host -p $psql_port -d $db_name -U $psql_user -c "$insert_stmt"
+exit $?
+
