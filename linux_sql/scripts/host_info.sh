f17494819714f0ce5c135627bb1e9dc607073d11
Roman Singh <romansingh.dev@gmail.com>
2022-01-05 00:16:24 +0000

    2

--- linux_sql/scripts/host_info.sh
+++ linux_sql/scripts/host_info.sh
@@ -8,8 +8,8 @@ psql_user=$4
 psql_password=$5
 
 #check number of arguments
-if [ $# -ne 5]; then
-  echo "Error: enter the correct number of args"
+if [ $# -ne 5 ]; then
+  echo "Enter the correct number of args."
   exit 1
 fi
 
@@ -22,7 +22,7 @@ mem_info=$(cat /proc/meminfo)
 
 #Hardware specification variables
 hostname=$(hostname -f)
-cpu_number= $(echo "$lscpu_out"  | egrep "^CPU\(s\):" | awk '{print $2}' | xargs)
+cpu_number=$(echo "$lscpu_out" | grep -E "^CPU\(s\):" | awk '{print $2}' | xargs)
 cpu_architecture= $(echo "$lscpu_out"  | egrep "Architecture:" | awk '{print $2}' | xargs)
 cpu_model= $(echo "$lscpu_out"  | egrep "^Model name:" | awk '{print $3 $4 $5 $6 $7}' | xargs)
 cpu_mhz= $(echo "$lscpu_out"  | egrep "^CPU MHz:" | awk '{print $3}' | xargs)
