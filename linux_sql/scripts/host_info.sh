6bf3bc64df310fe91d59b5387466b0b806a5a0c2
Roman Singh <romansingh.dev@gmail.com>
2022-01-05 01:05:24 +0000

    edit

--- linux_sql/scripts/host_info.sh
+++ linux_sql/scripts/host_info.sh
@@ -13,35 +13,30 @@ if [ $# -ne 5 ]; then
   exit 1
 fi
 
-#save number of CPU to a variable
-lscpu_out=`lscpu`
-
-#Save machine statistics
-vmstat_mb=$(vmstat --unit M)
+#save machine statiatic variable
+lscpu_out=$(lscpu)
 mem_info=$(cat /proc/meminfo)
 
 #Hardware specification variables
 hostname=$(hostname -f)
 cpu_number=$(echo "$lscpu_out" | grep -E "^CPU\(s\):" | awk '{print $2}' | xargs)
-cpu_architecture= $(echo "$lscpu_out"  | egrep "Architecture:" | awk '{print $2}' | xargs)
-cpu_model= $(echo "$lscpu_out"  | egrep "^Model name:" | awk '{print $3 $4 $5 $6 $7}' | xargs)
-cpu_mhz= $(echo "$lscpu_out"  | egrep "^CPU MHz:" | awk '{print $3}' | xargs)
-l2_cache= $(echo "$lscpu_out"  | egrep "^L2 cache:" | awk '{print $3}' | xargs)
-total_mem=$(echo "$mem_info"  | egrep "^MemTotal:" | awk '{print $2}' | xargs)
+cpu_architecture= $(echo "$lscpu_out"  | grep -E "Architecture:" | awk '{print $2}' | xargs)
+cpu_model= $(echo "$lscpu_out"  | grep -E "^Model name:" | awk '{print $3 $4 $5 $6 $7}' | xargs)
+cpu_mhz= $(echo "$lscpu_out"  | grep -E "^CPU MHz:" | awk '{print $3}' | xargs)
+l2_cache= $(echo "$lscpu_out"  | grep -E "^L2 cache:" | awk '{print $3}' | xargs)
+total_mem=$(echo "$mem_info"  | grep -E "^MemTotal:" | awk '{print $2}' | xargs)
 timestamp= $(vmstat -t | tail -n1 | awk '{print $18, $19}' | xargs)
 
-#Find matching id in host_info table
-host_id="(SELECT id FROM host_info WHERE hostname='$hostname')";
+
 
 #Insert into PSQL host_info.
-insert_stmt="INSERT INTO PUBLIC.host_info(timestamp, total_mem, l2_cache, cpu_mhz, cpu_model, cpu_architecture, cpu_number, hostname )
-VALUES('$timestamp', '$total_mem', '$l2_cache', '$cpu_mhz', '$cpu_model', '$cpu_architecture', '$cpu_number', '$hostname')"
+insert_stmt="INSERT INTO PUBLIC.host_info(timestamp, total_mem, l2_cache, cpu_mhz, cpu_model, cpu_architecture, cpu_number, hostname)VALUES('$timestamp', '$total_mem', '$l2_cache', '$cpu_mhz', '$cpu_model', '$cpu_architecture', '$cpu_number', '$hostname')"
 
 #env variable for psql command
 export PGPASSWORD=$psql_password
 
 #Insert data into a database
-psql -h $psql -p $psql_port -d $db_name -U $psql_user -c "insert_stmt"
+psql -h "$psql_host" -p "$psql_port" -d "$db_name" -U "$psql_user" -c "$insert_stmt"
 exit $?
 
 
