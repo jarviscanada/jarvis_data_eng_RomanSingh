2f30f7533d8c6073f77cb7416fefd98d14638cce
Roman Singh <romansingh.dev@gmail.com>
2022-01-05 01:11:54 +0000

    small edits

--- linux_sql/scripts/host_info.sh
+++ linux_sql/scripts/host_info.sh
@@ -20,12 +20,13 @@ mem_info=$(cat /proc/meminfo)
 #Hardware specification variables
 hostname=$(hostname -f)
 cpu_number=$(echo "$lscpu_out" | grep -E "^CPU\(s\):" | awk '{print $2}' | xargs)
-cpu_architecture= $(echo "$lscpu_out"  | grep -E "Architecture:" | awk '{print $2}' | xargs)
-cpu_model= $(echo "$lscpu_out"  | grep -E "^Model name:" | awk '{print $3 $4 $5 $6 $7}' | xargs)
-cpu_mhz= $(echo "$lscpu_out"  | grep -E "^CPU MHz:" | awk '{print $3}' | xargs)
-l2_cache= $(echo "$lscpu_out"  | grep -E "^L2 cache:" | awk '{print $3}' | xargs)
-total_mem=$(echo "$mem_info"  | grep -E "^MemTotal:" | awk '{print $2}' | xargs)
-timestamp= $(vmstat -t | tail -n1 | awk '{print $18, $19}' | xargs)
+cpu_architecture=$(echo "$lscpu_out" | grep -E "^Architecture:" | awk '{print $2}' | xargs)
+cpu_model=$(echo "$lscpu_out" | grep -E "^Model name:" | awk '{print $3 $4 $5 $6 $7}' | xargs)
+cpu_mhz=$(echo "$lscpu_out" | grep -E "^CPU MHz:" | awk '{print $3}' | xargs)
+l2_cache=$(echo "$lscpu_out" | grep -E "^L2 cache:" | awk '{print $3}' | xargs)
+l2_cache="${l2_cache::-1}"
+total_mem=$(echo "$mem_info" | grep -E "^MemTotal:" | awk '{print $2}' | xargs)
+timestamp=$(vmstat -t | tail -n1 | awk '{print $18, $19}' | xargs)
 
 
 
@@ -39,8 +40,15 @@ cpu_model,
 cpu_architecture,
 cpu_number,
 hostname)
-VALUES('$timestamp', '$total_mem',
-'$l2_cache', '$cpu_mhz', '$cpu_model', '$cpu_architecture', '$cpu_number', '$hostname')"
+VALUES('
+$timestamp',
+'$total_mem',
+'$l2_cache',
+'$cpu_mhz',
+'$cpu_model',
+'$cpu_architecture',
+'$cpu_number',
+'$hostname')"
 
 #env variable for psql command
 export PGPASSWORD=$psql_password
